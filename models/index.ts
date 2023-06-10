530043eb81d21e233d43e711311948540d628217
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-06-10 16:48:47 -0400

    Remove commented lines from models index file

--- models/index.ts
+++ models/index.ts
@@ -50,37 +50,3 @@ db.User = user(sequelize, Sequelize);
 
 
 export default db;
-
-
-
-
-
-
-
-
-// const dbConfig = require('../config/db.config.js');
-
-
-
-// // sequalize (lwrcase) is the instance of the package. So I guess we can call it anything.
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER,
-//     dbConfig.PASSWORD, {
-//     host: dbConfig.HOST,
-//     dialect: dbConfig.dialect,
-//     operationsAliases: false,
-//     pool: {
-//         max: dbConfig.pool.max,
-//         min: dbConfig.pool.min,
-//         acquire: dbConfig.pool.acquire,
-//         idle: dbConfig.pool.idle
-//     }
-// });
-// const db = {};
-
-// db.Sequelize = Sequelize;
-// db.sequelize = sequelize;
-
-// db.tutorials = require('./tutorial.model.js')(sequelize, Sequelize);
-
-// module.exports = db;
-
